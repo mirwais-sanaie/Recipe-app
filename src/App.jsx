c4511f3fbba34f28e23a316f4145a12fcbbca2d0
Mirwais <mirwais.sanaie@gmail.com>
2024-10-02 22:02:07 +0430

    project configration completed.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="bg-red-600 text-center mt-2">
+      I will create a recipe . the project configration finished successfully
+    </div>
+  );
+}
