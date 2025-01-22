b342883e3ff24c0b04c5870ef3b486149638553e
Mirwais <mirwais.sanaie@gmail.com>
2025-01-22 22:38:17 +0430

    details page completed.

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useGlobalContext } from "../contexts/GlobalContext";
+import Loading from "../components/Loading";
 
 function Details() {
-  const urlParams = useParams();
-  const { loading, setLoading } = useGlobalContext();
+  const { id } = useParams();
+  const { loading, setLoading, detailRecipe, setDetailRecipe } =
+    useGlobalContext();
 
   useEffect(
     function () {
@@ -12,10 +14,11 @@ function Details() {
         try {
           setLoading(true);
           const res = await fetch(
-            `https://forkify-api.herokuapp.com/api/v2/recipes/${urlParams}`
+            `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
           );
           const recipe = await res.json();
-          console.log(recipe);
+          setDetailRecipe(recipe);
+          console.log(recipe.data);
         } catch (e) {
           console.log(e);
         } finally {
@@ -25,14 +28,51 @@ function Details() {
       recipeDetail();
     },
 
-    [setLoading, urlParams]
+    [setLoading, id, setDetailRecipe]
   );
 
-  if (loading) return <div>Loading...</div>;
+  if (loading)
+    return (
+      <div className="flex justify-center mt-60">
+        <Loading />
+      </div>
+    );
 
   return (
-    <div>
-      <h1>Details page</h1>
+    <div className="container mx-auto mt-12">
+      <div className="flex">
+        <div className="rounded-lg group overflow-hidden">
+          <img
+            className="group-hover:scale-105 object-cover duration-200"
+            src={detailRecipe?.data?.recipe?.image_url}
+            alt=""
+          />
+        </div>
+        <div className="ms-10 space-y-4">
+          <p className="text-blue-600">
+            {detailRecipe?.data?.recipe?.publisher}
+          </p>
+          <h1 className="text-2xl font-black mb-5">
+            {detailRecipe?.data.recipe?.title}
+          </h1>
+          <button className="w-full bg-blue-500 text-white py-3 rounded-lg hover:bg-blue-600">
+            Add to List
+          </button>
+        </div>
+      </div>
+      {/* Ingredients Section */}
+      <div className="mt-12">
+        <h2 className="text-2xl font-bold mb-4">Ingredients</h2>
+        <ol className="list-decimal grid grid-cols-3 gap-y-4">
+          {detailRecipe?.data?.recipe?.ingredients.map((ingredient, index) => (
+            <li key={index} className="text-gray-700">
+              {ingredient.quantity && <span>{ingredient.quantity} </span>}
+              {ingredient.unit && <span>{ingredient.unit} </span>}
+              <span>{ingredient.description}</span>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
